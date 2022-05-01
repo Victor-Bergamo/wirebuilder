18597fa28f2dfadc93179a447d6fcbb8cf22e93c
Victor-Bergamo <victorhbbergamo@gmail.com>
2022-05-01 03:49:14 -0300

    Package start

--- wirebuilder/packages/coffeemosele/wirebuilder/resources/copytest/components/form/field/button.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="col-span-6 sm:col-span-3">
-    <div class="flex-1">
-        {{-- <label for="{!! $idField !!}" class="block text-sm font-medium text-gray-700"></label> --}}
-        <button type="button" class="self-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"> a </button>
-    </div>
-</div>
